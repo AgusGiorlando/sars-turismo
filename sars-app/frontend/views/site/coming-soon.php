2a667f01411c0075c423543970805af0385e198f
AgusGiorlando <agus.giorlando@gmail.com>
2021-04-13 17:32:57 -0300

    Coming soon reordenado

--- sars-app/frontend/views/site/coming-soon.php
+++ sars-app/frontend/views/site/coming-soon.php
@@ -15,21 +15,15 @@ $imgUrl = Yii::getAlias('@webroot/img/site/logo.jpg');
 ?>
 <div>
     <section id="coming-soon" class="coming-soon">
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-3">En construcción</h1>
-                <p class="lead">Estamos trabajando en nuestro nuevo sitio web...Pronto estará listo!</p>
-                <hr class="my-2 text-center">
-                <p class="lead text-center">
-                    <?php echo Icon::show('phone') . ' ' . Yii::$app->params['contactPhoneNumber']; ?><br>
-                    <?php echo Icon::show('envelope') . ' ' . Yii::$app->params['contactEmail']; ?><br> <br>
-                    <a href="<?= Yii::$app->params['instagramUrl'] ?>" class="instagram"><i class="bx bxl-instagram bx-md"></i></a>
-                    <a href="<?= Yii::$app->params['facebookUrl'] ?>" class="facebook"><i class="bx bxl-facebook bx-md"></i></a>
-                </p>
-            </div>
+        <div class="logo text-center">
+            <?= Html::img('/img/site/logo.jpg', ['class' => 'img']); ?>
+            <h1 class="title">EN CONSTRUCCIÓN</h1>
+            <p class="lead"><b>Estamos trabajando en nuestro nuevo sitio web... <br>Muy pronto estará listo!</b></p>
         </div>
-        <div class="logo">
-                <?= Html::img('/img/site/logo.jpg', ['class' => 'img']); ?>
+        <div class="container text-center">
+            <i class="bx bxl-whatsapp"></i><?php echo ' ' . Yii::$app->params['contactPhoneNumber']; ?><br>
+            <?php echo Icon::show('envelope') . ' ' . Yii::$app->params['contactEmail']; ?><br> <br>
+            <a href="<?= Yii::$app->params['instagramUrl'] ?>" class="instagram"><i class="bx bxl-instagram bx-md"></i></a>
+            <a href="<?= Yii::$app->params['facebookUrl'] ?>" class="facebook"><i class="bx bxl-facebook bx-md"></i></a>
         </div>
-
 </div>
