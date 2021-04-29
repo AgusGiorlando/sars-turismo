e03010b56324b436069ba31b6cfdf0d4acb62f1a
AgusGiorlando <agus.giorlando@gmail.com>
2021-04-28 23:19:39 -0300

    Portada en home

--- sars-app/frontend/views/site/index.php
+++ sars-app/frontend/views/site/index.php
@@ -9,77 +9,78 @@ $this->title = 'SARS Turismo';
   <section id="hero" class="d-flex align-items-center">
 
     <div class="container d-flex flex-column align-items-center justify-content-center" data-aos="fade-up">
-      <img src='/img/site/logo.jpg' class="img-fluid hero-img" alt="" data-aos="zoom-in" data-aos-delay="150">
+      <img src='/img/site/Puzzle-completo-alta.jpg' class="img-fluid hero-img" alt="" data-aos="zoom-in" data-aos-delay="150">
+      <!-- <img src='/img/site/logo.jpg' class="img-fluid hero-img" alt="" data-aos="zoom-in" data-aos-delay="150"> -->
       <h1>EXPLORÁ, ESCUCHÁ, SENTÍ, SABOREÁ, DISFRUTÁ, DESCUBRÍ, VIAJÁ</h1>
-
       <!-- <a href="#about" class="btn-get-started scrollto">Get Started</a> -->
     </div>
 
   </section><!-- End Hero -->
 
   <main id="main">
-      <!-- ======= Services Section ======= -->
-      <section id="services" class="services">
-        <div class="container" data-aos="fade-up">
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
 
-          <div class="section-title">
-            <h2>Actividades</h2>
-            <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
-          </div>
-
-          <div class="row">
-            <div class="col-md" data-aos="fade-up" data-aos-delay="100">
-              <div class="icon-box">
-                <div class="icon"><i class="bx bx-bus"></i></div>
-                <h4 class="title"><a href="">Paseos</a></h4>
-                <!-- <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p> -->
-              </div>
-            </div>
-
-            <div class="col-md" data-aos="fade-up" data-aos-delay="200">
-              <div class="icon-box">
-                <div class="icon"><i class="bx bx-landscape"></i></div>
-                <h4 class="title"><a href="">Aventura</a></h4>
-                <!-- <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p> -->
-              </div>
-            </div>
-
-            <div class="col-md" data-aos="fade-up" data-aos-delay="300">
-              <div class="icon-box">
-                <div class="icon"><i class="bx bx-list-check"></i></div>
-                <h4 class="title"><a href="">Paquetes</a></h4>
-                <!-- <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p> -->
-              </div>
-            </div>
-
-            <div class="col-md" data-aos="fade-up" data-aos-delay="400">
-              <div class="icon-box">
-                <div class="icon"><i class="bx bx-restaurant"></i></div>
-                <h4 class="title"><a href="">Gastronomía</a></h4>
-                <!-- <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p> -->
-              </div>
-            </div>
-
-            <div class="col-md" data-aos="fade-up" data-aos-delay="500">
-              <div class="icon-box">
-                <div class="icon"><i class="bx bx-building-house"></i></div>
-                <h4 class="title"><a href="">Alojamiento</a></h4>
-                <!-- <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p> -->
-              </div>
-            </div>
-          </div>
-
+        <div class="section-title">
+          <h2>Actividades</h2>
+          <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
         </div>
-      </section><!-- End Services Section -->
+
+        <div class="row">
+          <div class="col-md" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-bus"></i></div>
+              <h4 class="title"><a href="">Paseos</a></h4>
+              <!-- <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p> -->
+            </div>
+          </div>
+
+          <div class="col-md" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-landscape"></i></div>
+              <h4 class="title"><a href="">Aventura</a></h4>
+              <!-- <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p> -->
+            </div>
+          </div>
+
+          <div class="col-md" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-list-check"></i></div>
+              <h4 class="title"><a href="">Paquetes</a></h4>
+              <!-- <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p> -->
+            </div>
+          </div>
+
+          <div class="col-md" data-aos="fade-up" data-aos-delay="400">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-restaurant"></i></div>
+              <h4 class="title"><a href="">Gastronomía</a></h4>
+              <!-- <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p> -->
+            </div>
+          </div>
+
+          <div class="col-md" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-building-house"></i></div>
+              <h4 class="title"><a href="">Alojamiento</a></h4>
+              <!-- <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p> -->
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
     </section><!-- End About Section -->
 
     <!-- ======= Features Section ======= -->
     <section id="features" class="features" data-aos="fade-up">
       <div class="container">
-
         <div class="section-title">
           <h2>Sobre Nosotros</h2>
-          <h4><p>Sars es una empresa joven, que nació cuando el mundo entero se detuvo, cuando el mundo cambio para siempre. Nacio para desafiar la tradicional manera de viajar y hacer de un simple viaje de turismo una <b> EXPERIENCIA UNICA E INOLVIDABLE.</b></p></h4>
+          <h4>
+            <p>Sars es una empresa joven, que nació cuando el mundo entero se detuvo, cuando el mundo cambio para siempre. Nacio para desafiar la tradicional manera de viajar y hacer de un simple viaje de turismo una <b> EXPERIENCIA UNICA E INOLVIDABLE.</b></p>
+          </h4>
         </div>
 
         <div class="row content">
@@ -89,9 +90,9 @@ $this->title = 'SARS Turismo';
         </div>
         <div class="row content d-flex flex-row-reverse">
           <div class="col-md-7 pt-1 order-2 order-md-1" data-aos="fade-right">
-            <h3><b>Sars Turismo </b>tiene su propia  filosofía, hacemos que nuestros pasajeros sean los protagonistas en cada lugar que estén.</h3>
+            <h3><b>Sars Turismo </b>tiene su propia filosofía, hacemos que nuestros pasajeros sean los protagonistas en cada lugar que estén.</h3>
             <p>
-            Creamos itinerarios de acuerdo a los gustos y a cada sensación que cada uno de ellos se quiera llevar en ese viaje y permitiendo así una profunda interacción con los paisajes, las culturas y costumbres locales.
+              Creamos itinerarios de acuerdo a los gustos y a cada sensación que cada uno de ellos se quiera llevar en ese viaje y permitiendo así una profunda interacción con los paisajes, las culturas y costumbres locales.
             </p>
           </div>
         </div>
