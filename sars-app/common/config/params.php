669ac7972882cbbf38696f87502a504a4eab5544
AgusGiorlando <agus.giorlando@gmail.com>
2021-07-06 17:23:41 -0300

    tripadvisor logo

--- sars-app/common/config/params.php
+++ sars-app/common/config/params.php
@@ -7,7 +7,8 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
     'contactPhoneNumber' => '+5492614723149',
-    'contactEmail' => 'sarsturismo@gmail.com',
+    'contactEmail' => 'reservas@sarsturismo.com.ar',
     'instagramUrl' => 'https://www.instagram.com/sarsturismo',
-    'facebookUrl' => 'https://www.facebook.com/sars.turismo/'
+    'facebookUrl' => 'https://www.facebook.com/sars.turismo/',
+    'tripadvisorUrl' => 'https://www.tripadvisor.es/Attraction_Review-g312781-d23187446-Reviews-SARS_TURISMO-Mendoza_Province_of_Mendoza_Cuyo.html?m=19905'
 ];
