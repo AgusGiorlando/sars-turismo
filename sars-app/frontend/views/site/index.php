e04b875274dd7fbea1fd8206ebaeb394f9808a70
AgusGiorlando <agus.giorlando@gmail.com>
2021-07-12 17:20:10 -0300

    visual fixes

--- sars-app/frontend/views/site/index.php
+++ sars-app/frontend/views/site/index.php
@@ -39,55 +39,52 @@ $this->title = 'SARS Turismo';
         </a>
       </div>
     </div>
+
+    <!--TOURS PROMOCIONADOS-->
     <div class="activities-menu" data-aos="fade-up">
       <div class="row row-orange d-flex justify-content-center">
         <div class="col-md-2 d-flex category" data-aos="fade-up" data-aos-delay="100">
           <div class="row d-flex justify-content-center">
-            <div class="icon"><img src='/img/site/services-logos/SARS _5_.jpg' class="activities-logo"> </div>
+            <div class="icon"><img src='/img/site/services-logos/SARS _2_.jpg' class="activities-logo"> </div>
             <h2>Paseos</h2>
           </div>
         </div>
-        <div class="col-sm" data-aos="fade-up" data-aos-delay="100">
-          <div class="icon-box">
-            <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
-            <h4><?= Bootstrap4Html::a('Aconcagua', Url::to(['service/view', 'id' => 'd05f499a-f339-312a-809f-cd8a69ad37c4']), ['class' => 'title']); ?></h4>
-            <span>Desde AR$ 45000</span>
+        <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="200">
+          <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
+          <h4><?= Bootstrap4Html::a('Aconcagua', Url::to(['service/view', 'id' => 'd05f499a-f339-312a-809f-cd8a69ad37c4']), ['class' => 'title']); ?></h4>
+          <span>Desde AR$ 45000</span>
+        </div>
+      </div>
+      <div class="row row-grey d-flex justify-content-center">
+        <div class="col-md-2 d-flex category" data-aos="fade-up" data-aos-delay="0">
+          <div class="row d-flex justify-content-center">
+            <div class="icon"><img src='/img/site/services-logos/SARS _2_.jpg' class="activities-logo"> </div>
+            <h2>Aventura</h2>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="row row-grey d-flex justify-content-center">
-      <div class="col-md-2 d-flex category" data-aos="fade-up" data-aos-delay="100">
-        <div class="row d-flex justify-content-center">
-          <div class="icon"><img src='/img/site/services-logos/SARS _2_.jpg' class="activities-logo"> </div>
-          <h2>Aventura</h2>
+        <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="200">
+          <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
+          <h4 class="title"><a href="">Aconcagua</a></h4>
+          <span>Desde AR$ 45000</span>
+        </div>
+        <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="300">
+          <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
+          <h4 class="title"><a href="">Aconcagua</a></h4>
+          <span>Desde AR$ 45000</span>
+        </div>
+        <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="400">
+          <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
+          <h4 class="title"><a href="">Aconcagua</a></h4>
+          <span>Desde AR$ 45000</span>
+        </div>
+        <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="500">
+          <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
+          <h4 class="title"><a href="">Aconcagua</a></h4>
+          <span>Desde AR$ 45000</span>
         </div>
       </div>
-      <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="100">
-        <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
-        <h4 class="title"><a href="">Aconcagua</a></h4>
-        <span>Desde AR$ 45000</span>
-      </div>
-      <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="100">
-        <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
-        <h4 class="title"><a href="">Aconcagua</a></h4>
-        <span>Desde AR$ 45000</span>
-      </div>
-      <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="100">
-        <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
-        <h4 class="title"><a href="">Aconcagua</a></h4>
-        <span>Desde AR$ 45000</span>
-      </div>
-      <div class="col-sm-2 icon-box" data-aos="fade-up" data-aos-delay="100">
-        <div class="icon"><img src='/img/site/WhatsApp Image 2021-04-06 at 13.27.40.jpeg' class="activities-img"> </div>
-        <h4 class="title"><a href="">Aconcagua</a></h4>
-        <span>Desde AR$ 45000</span>
-      </div>
     </div>
 </div>
-</div>
-
-
 </section><!-- End Hero -->
 
 <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
